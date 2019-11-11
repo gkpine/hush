99fd05e60964e9d5abca4314c366fc446ea42d62
Dan <dgagnonk@ryerson.ca>
2019-11-11 00:48:22 -0500

    react-quill added and sort of styled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.bubble.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ReactQuill className="editor" theme="bubble"/>
+    </div> 
   );
 }
 
